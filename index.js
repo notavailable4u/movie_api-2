94d0073dd7b87e5bcb766b4e516347b06fe341df
notavailable4u <139932183+notavailable4u@users.noreply.github.com>
2024-05-16 16:33:03 -0500

    Modify CORS

--- index.js
+++ index.js
@@ -13,7 +13,19 @@ app.use(express.urlencoded({ extended: true }));
 const { check, validationResult } = require('express-validator');
 
 const cors = require('cors');
-app.use(cors());
+// app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://movie-api-pgannon.netlify.app'];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 // Import and configure authentication
 // eslint-disable-next-line no-unused-vars
